90de86eb1b5f397aa6c4a880ae5458ab6b9e74e7
Johan Bjerkem <bjerkem.j@gmail.com>
2023-04-11 11:20:42 +0200

    finish test_eventManager.py

--- tests/test_eventManager.py
+++ tests/test_eventManager.py
@@ -7,6 +7,13 @@ sys.path.insert(0, ROOT+"/..")
 from eventManager import TestObject, Event, EventQueue
 
 class TestEvent(unittest.TestCase):
+    def setUp(self):
+        self.old_stdout = sys.stdout
+        sys.stdout = self.buffer = io.StringIO()
+
+    def tearDown(self) -> None:
+        sys.stdout = self.old_stdout
+
     def test_init_and_execute_event(self):
         test_obj = TestObject()
         event = Event(5, test_obj, test_obj.fun, "parameter 1", "parameter 2")
@@ -15,14 +22,10 @@ class TestEvent(unittest.TestCase):
         self.assertEqual(event.object, test_obj)
         self.assertEqual(event.function, test_obj.fun)
         self.assertEqual(event.functionParameters, ("parameter 1", "parameter 2"))
-
-        old_stdout = sys.stdout
-        sys.stdout = buffer = io.StringIO()
         
         event.executeEvent()
 
-        sys.stdout = old_stdout
-        log = buffer.getvalue()
+        log = self.buffer.getvalue()
         
         expected_logs = "time is 5\nparameter 1 parameter 2\n"
         self.assertEqual(log, expected_logs)
@@ -33,6 +36,13 @@ class TestEvent(unittest.TestCase):
         self.assertEqual(repr(event), "Event at time 5 on object <eventManager.TestObject object at ADDRESS> is function fun with args ('parameter 1', 'parameter 2')".replace("ADDRESS", hex(id(test_obj))))
 
 class TestEventQueue(unittest.TestCase):
+    def setUp(self):
+        self.old_stdout = sys.stdout
+        sys.stdout = self.buffer = io.StringIO()
+
+    def tearDown(self) -> None:
+        sys.stdout = self.old_stdout
+
     def test_init(self):
         eq = EventQueue()
         self.assertEqual(eq.time, 0)
@@ -68,7 +78,11 @@ class TestEventQueue(unittest.TestCase):
 
     def test_has_next_event(self):
         eq = EventQueue()
+        test_obj = TestObject()
         self.assertFalse(eq.hasNextEvent())
+
+        eq.createAndQueueEvent(3, test_obj, test_obj.fun, "parameter 1", "parameter 2")
+        self.assertTrue(eq.hasNextEvent())
 
     def test_execute_next_event(self):
         eq = EventQueue()
@@ -79,14 +93,10 @@ class TestEventQueue(unittest.TestCase):
         eq.queueEvent(event1)
         eq.queueEvent(event2)
 
-        old_stdout = sys.stdout
-        sys.stdout = buffer = io.StringIO()
-
         eq.executeNextEvent()
         eq.executeNextEvent()
 
-        sys.stdout = old_stdout
-        log = buffer.getvalue()
+        log = self.buffer.getvalue()
 
         expected_logs = "time is 1\nparameter 1 parameter 2\ntime is 2\nparameter 3 parameter 4\n"
         self.assertEqual(log, expected_logs)
